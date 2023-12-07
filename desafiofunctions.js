b0159c75b792ada1b79395b8a98e84cd5072bbe6
anajulialobato <anajuliamnzs19@gmail.com>
2023-12-07 16:27:19 -0300

    desafio 2

---
+++ desafiofunctions.js
@@ -0,0 +1,27 @@
+function calculatorRankeadas(numW, numL){
+    let winLosses = (numW - numL)
+    return winLosses
+}
+let levelHero = calculatorRankeadas(45, 11)
+let nivel = ""
+    
+
+    if (levelHero <= 10){
+        nivel = "ferro"
+    }
+    else if (levelHero>11 && levelHero<=20){
+        nivel = "bronze"
+    }
+    else if (levelHero>21 && levelHero<=50){
+        nivel = "prata"
+    }
+    else if (levelHero>81 && levelHero<=90){
+        nivel = "ouro"
+    }
+    else if (levelHero>91 && levelHero<=100){
+        nivel = "lendário"
+    }
+    else{
+        nivel = "imortal"
+    }
+console.log("O herói tem saldo de " + levelHero + " está no nível de " + nivel)
